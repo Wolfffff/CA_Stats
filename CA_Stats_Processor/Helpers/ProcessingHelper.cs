8aa01c71ac45489d8d556644fe04606ac39e59cb
swwolf@ualr.edu <swwolf@ualr.edu>
2018-06-21 18:35:46 -0400

    Initial check-in of module CA_Stats_Processor

---
+++ CA_Stats_Processor/Helpers/ProcessingHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace CA_Stats_Processor.Helpers
+{
+    //If we want to optimize it, we can put all of this into a single function but because the low complexity, there's no reason to worry about processing speedcat 
+    public class ProcessingHelper
+    {
+
+        public static Tuple<int,int> ThirtyAndSixty(string[,] array){
+            var thirties = 0;
+            var sixties = 0;
+            var startTime = Convert.ToDateTime(array[0, 0]);
+            var started = false;
+            var csv = new StringBuilder();
+
+
+            //after your loop
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                if (array[i, 1] != "COMPLETE")
+                {
+                    if(!started){
+                        started = true;
+                        //These if cover if the first entry is not valid
+                        if(array[i-1,0]!= null){ 
+                            startTime = Convert.ToDateTime(array[i - 1, 0]);
+
+                        }
+                        else if (array[i - 1, 0] == null){
+                            startTime = Convert.ToDateTime(array[i, 0]);
+                        }
+
+                    }
+
+
+                }else if(array[i,1] == "COMPLETE"){
+                    if(started){
+                        started = false;
+                        if((Convert.ToDateTime(array[i, 0]) - startTime).TotalMinutes > 30){
+                thirties += 1;
+                            var newLine = string.Format("{0},{1},{2}", startTime, Convert.ToDateTime(array[i,0]), 30);
+                            csv.AppendLine(newLine);
+                        }
+                        if ((Convert.ToDateTime(array[i, 0]) - startTime).TotalMinutes > 60)
+                        {
+                            var newLine = string.Format("{0},{1},{2}", startTime, Convert.ToDateTime(array[i, 0]), 60);
+                            csv.AppendLine(newLine);
+                            sixties += 1;
+                        }
+                    }
+                }
+            }
+            File.WriteAllText(@"Output.csv", csv.ToString());
+            return new Tuple<int, int>(thirties, sixties);
+        }
+    }
+}
---
+++ CA_Stats_Processor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CA_Stats_Processor.Helpers;
+
+namespace CA_Stats_Processor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Entergy CA Stats CSV Processor \n \n");
+            Console.Write("Please type exact directory of CSV files. \nNote: folder should container only relevant CSV files with proper names.\n");
+            var location = Console.ReadLine();
+            var array = CSVHelper.CombineAndConvert(location);
+
+
+            var largeGapCountAndMaxGap = ProcessingHelper.ThirtyAndSixty(array);
+            Console.Write("Number of gaps with time(in minutes) greater than 30 is " + largeGapCountAndMaxGap.Item1 + ".\n\n");
+            Console.Write("Number of gaps with time(in minutes) greater than 60 is " + largeGapCountAndMaxGap.Item2 + ".\n");
+
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
